8618aa048ce4e804e3b92c5fd536d6d5d6ccd115
Moon Seongmu <76660830+moonseongmu@users.noreply.github.com>
2026-07-17 23:23:35 +0800

    use mult_array function in lock_in

--- tufo_core/Src/lockin.c
+++ tufo_core/Src/lockin.c
@@ -29,15 +29,13 @@ void lock_in(uint16_t *sin_buffer, uint16_t *cos_buffer, uint16_t *signal_buffer
     uint32_t X_buffer[block_size];
     uint32_t Y_buffer[block_size];
 
-    for(uint32_t i = 0; i < block_size; i++)
-    {
-        in_phase_buffer[i] = cos_buffer[i] * signal_buffer[i];
-        quadrature_buffer[i] = sin_buffer[i] * signal_buffer[i];
+    mult_array(cos_buffer, signal_buffer, in_phase_buffer, block_size);
+    mult_array(sin_buffer, signal_buffer, quadrature_buffer, block_size);
 
-        low_pass_filter(in_phase_buffer, X_buffer);
-        low_pass_filter(quadrature_buffer, Y_buffer);
+    low_pass_filter(in_phase_buffer, X_buffer);
+    low_pass_filter(quadrature_buffer, Y_buffer);
 
-        rect_to_polar(X_buffer, Y_buffer, amplitude_buffer, phase_buffer, block_size);
-    }
+    rect_to_polar(X_buffer, Y_buffer, amplitude_buffer, phase_buffer, block_size);
+
 }
 
