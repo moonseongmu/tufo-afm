f0ffe165f80336250daf0d41261d43afa7217113
Moon Seongmu <76660830+moonseongmu@users.noreply.github.com>
2026-07-18 23:02:40 +0800

    added block size arg to low pass filter func prototype

--- tufo_core/Inc/lockin.h
+++ tufo_core/Inc/lockin.h
@@ -10,6 +10,6 @@ uint16_t adc_in_buffer_1[BLOCK_SIZE];
 uint16_t adc_in_buffer_2[BLOCK_SIZE];
 
 void mult_array(uint16_t *src_1, uint16_t *src_2, uint32_t *dest, uint32_t block_size);
-void low_pass_filter(uint32_t *src, uint32_t *dest);
+void low_pass_filter(uint32_t *src, uint32_t *dest, uint32_t block_size);
 void rect_to_polar(uint32_t *X, uint32_t *Y, float *modulus, float *phase, uint32_t block_size);
 void lock_in(uint16_t *sin_buffer, uint16_t *cos_buffer, uint16_t *signal_buffer, float *amplitude_buffer, float *phase_buffer, uint32_t block_size);
