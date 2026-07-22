8ef420fea52d308d5c40df8bb523494eebe14ff3
Moon Seongmu <76660830+moonseongmu@users.noreply.github.com>
2026-07-22 22:30:24 +0800

    implemented more of freq_sweep module

--- tufo_core/Src/freq_sweep.c
+++ tufo_core/Src/freq_sweep.c
@@ -1,16 +1,32 @@
 #include "freq_sweep.h"
 
-void set_sweep_settings(sweep_settings_t *settings, float start_freq, float end_freq, float step)
+float mean(float *array, uint32_t size)
+{
+    float sum = 0;
+    for (uint32_t i = 0; i < size; i++)
+    {
+        sum += array[i];
+    }
+    return sum/size;
+}
+
+void set_sweep_settings(sweep_settings_t *settings, float start_freq, float end_freq, float step, float max_epsilon)
 {
     settings->prev_amplitude = 0;
     settings->start_freq = start_freq;
     settings->end_freq = end_freq;
     settings->step_size = step;
+    settings->max_epsilon = max_epsilon;
 }
 
-void start_oscillations(sweep_settings_t *settings, dds_t *sin_dds, dds_t *cos_dds, uint32_t block_size, active_buffer_t active_buffer,
+void start_oscillations(sweep_settings_t *settings, 
+    dds_t *sin_dds, dds_t *cos_dds, 
+    uint32_t block_size, active_buffer_t active_buffer, uint32_t *counter, bool buffers_swapped,
     uint16_t *sin_buf_1, uint16_t *sin_buf_2, 
-    uint16_t *cos_buf_1, uint16_t *cos_buf_2)
+    uint16_t *cos_buf_1, uint16_t *cos_buf_2, 
+    uint16_t *sig_buf_1, uint16_t *sig_buf_2,
+    float *ampl_buf_1, float *ampl_buf_2, 
+    float *phase_buf_1, float *phase_buf_2)
 {
     //start timer and dma to adc and dac, interrupt calls when buffer filled/finished outputing
     block_transfer_init();
@@ -20,32 +36,78 @@ void start_oscillations(sweep_settings_t *settings, dds_t *sin_dds, dds_t *cos_d
 
     //init sindds with sweep starting frequency + starting phase 90
     sin_dds->tuning_word = freq_to_tuning_word(settings->start_freq, settings->clock_freq);
-    sin_dds->phase_accumulator = 1073741824; //(2^32-1)/4
+    sin_dds->phase_accumulator = 1073741824; //(2^32-1)/4 for 90* initial starting offset phase
+    settings->current_freq = settings->start_freq;
 
     block_transfer_start();
+    float amplitude_avg = 0.0f;
     
-    for(int i = 0; i < 6; i++ )
+    while(buffers_swapped == true && *counter < 6)
     {
-        fill_sin_cos_buffers(sin_dds, cos_dds, block_size, active_buffer, sin_buf_1, sin_buf_2, cos_buf_1, cos_buf_2);
+        switch(active_buffer)
+        {
+            case buffer_1:
+                lock_in(sin_buf_1, cos_buf_1, sig_buf_1, ampl_buf_1, phase_buf_1, block_size);
+                amplitude_avg = mean(ampl_buf_1, block_size);
+                fill_sin_cos_buffers(sin_dds, cos_dds, block_size, sin_buf_1, cos_buf_1);
+                break;
+            case buffer_2:
+                lock_in(sin_buf_2, cos_buf_2, sig_buf_2, ampl_buf_2, phase_buf_2, block_size);
+                amplitude_avg = mean(ampl_buf_2, block_size);
+                fill_sin_cos_buffers(sin_dds, cos_dds, block_size, sin_buf_2, cos_buf_2);
+                break;
+        }
+        settings->prev_amplitude = amplitude_avg;
+        *counter += 1;
+        buffers_swapped = false;
     }
 }
 
-void fill_sin_cos_buffers(dds_t *sin_dds, dds_t *cos_dds, uint32_t block_size, active_buffer_t active_buffer,
+
+void continue_oscillations(sweep_settings_t *settings, 
+    dds_t *sin_dds, dds_t *cos_dds, 
+    uint32_t block_size, active_buffer_t active_buffer, bool buffers_swapped,
     uint16_t *sin_buf_1, uint16_t *sin_buf_2, 
-    uint16_t *cos_buf_1, uint16_t *cos_buf_2)
+    uint16_t *cos_buf_1, uint16_t *cos_buf_2, 
+    uint16_t *sig_buf_1, uint16_t *sig_buf_2,
+    float *ampl_buf_1, float *ampl_buf_2, 
+    float *phase_buf_1, float *phase_buf_2)
+{
+    float amplitude_avg = 0.0f;
+    switch(active_buffer)
+    {
+        case buffer_1:
+            lock_in(sin_buf_1, cos_buf_1, sig_buf_1, ampl_buf_1, phase_buf_1, block_size);
+            amplitude_avg = mean(ampl_buf_1, block_size);
+            fill_sin_cos_buffers(sin_dds, cos_dds, block_size, sin_buf_1, cos_buf_1);
+            break;
+        case buffer_2:
+            lock_in(sin_buf_2, cos_buf_2, sig_buf_2, ampl_buf_2, phase_buf_2, block_size);
+            amplitude_avg = mean(ampl_buf_2, block_size);
+            fill_sin_cos_buffers(sin_dds, cos_dds, block_size, sin_buf_2, cos_buf_2);
+            break;
+    }
+    
+    if(fabsf(amplitude_avg - settings->prev_amplitude) < settings->max_epsilon)
+    {
+        //amplitude has stabilised, move tuning word to next step
+        settings->current_freq += settings->step_size;
+        sin_dds->tuning_word = freq_to_tuning_word(settings->current_freq, settings->clock_freq);
+        cos_dds->tuning_word = freq_to_tuning_word(settings->current_freq, settings->clock_freq);
+    }
+    settings->prev_amplitude = amplitude_avg;
+    buffers_swapped = false;
+}
+
+void fill_sin_cos_buffers(dds_t *sin_dds, dds_t *cos_dds, uint32_t block_size, uint16_t *sin_buf, uint16_t *cos_buf)
 {
     for (uint32_t i=0; i<block_size; i++)
     {
         dds_calculate(sin_dds);
         dds_calculate(cos_dds);
-        switch (active_buffer)
-        {
-            case buffer_1:
-                cos_buf_1[i] = cos_dds->value;
-                sin_buf_1[i] = sin_dds->value;
-            case buffer_2:
-                cos_buf_2[i] = cos_dds->value;
-                sin_buf_2[i] = sin_dds->value;
-        }
+            cos_buf[i] = cos_dds->value;
+            sin_buf[i] = sin_dds->value;
     }
 }
+
+
--- tufo_core/Src/tufo_main.c
+++ tufo_core/Src/tufo_main.c
@@ -1,9 +1,6 @@
 #include "tufo_main.h"
-#include "dds.h"
-#include "freq_sweep.h"
 #include "oscillator.h"
-#include "stm32h723xx.h"
-
+#include <stdint.h>
 
 typedef enum state_t
 {
@@ -14,11 +11,19 @@ typedef enum state_t
     STATE_ACQUIRE
 } state_t;
 
+typedef enum command_t
+{
+    NONE,
+    COMMAND_START_SWEEP,
+    COMMAND_START_ACQUISITION
+} command_t;
+
 
 void tufo_main(void)
 {
 
     state_t state = STATE_IDLE;
+    command_t command = NONE;
 
     sweep_settings_t sweep_settings = {.sweep_complete = false};
     
@@ -40,30 +45,53 @@ void tufo_main(void)
     float phase_buffer_1[BLOCK_SIZE];
     float phase_buffer_2[BLOCK_SIZE];
 
+    uint32_t osc_init_counter = 0;
+
     while(1)
     {
         switch(state)
         {
-            case STATE_IDLE:
+            case STATE_IDLE: //TODO: implement idle case
 
-                if(buffers_swapped == true)
+                if (command == COMMAND_START_SWEEP)
+                {
+                    state = STATE_SWEEP_INIT;
+                }
+                if(buffers_swapped == true && sweep_settings.sweep_complete == false)
                 {
                     state = STATE_FREQ_SWEEP;
+                } else if(buffers_swapped == true && sweep_settings.sweep_complete == true)
+                {
+                    state = STATE_SWEEP_STOP;
                 }
                 break;
 
             case STATE_SWEEP_INIT:
-                set_sweep_settings(&sweep_settings, 32677, 32679, 0.1);
-                start_oscillations(&sweep_settings, &sin_dds, &cos_dds, BLOCK_SIZE, active_buffer, sin_buffer_1, sin_buffer_2, cos_buffer_1, cos_buffer_2);
+                set_sweep_settings(&sweep_settings, 32677, 32679, 0.1, 0.1);
+                start_oscillations(&sweep_settings, &sin_dds, &cos_dds, 
+                    BLOCK_SIZE, active_buffer, &osc_init_counter, buffers_swapped, 
+                    sin_buffer_1, sin_buffer_2, 
+                    cos_buffer_1, cos_buffer_2, 
+                    adc_in_buffer_1, adc_in_buffer_2, 
+                    amplitude_buffer_1, amplitude_buffer_2, 
+                    phase_buffer_1, phase_buffer_2);
                 sweep_settings.sweep_complete = false;
                 state = STATE_IDLE;
                 break;
 
             case STATE_FREQ_SWEEP:
+                continue_oscillations(&sweep_settings, &sin_dds, &cos_dds, 
+                    BLOCK_SIZE, active_buffer, buffers_swapped,
+                    sin_buffer_1, sin_buffer_2,
+                    cos_buffer_1, cos_buffer_2,
+                    adc_in_buffer_1, adc_in_buffer_2, 
+                    amplitude_buffer_1, amplitude_buffer_2,
+                    phase_buffer_1, phase_buffer_2);
                 break;
 
             case STATE_SWEEP_STOP:
                 break;
+
             case STATE_ACQUIRE:
                 break;
 
